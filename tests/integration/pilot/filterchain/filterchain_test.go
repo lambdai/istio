4936329f4a8a9dd7eff1e2461688532caa4500ce
Yuchen Dai <silentdai@gmail.com>
2019-04-04 18:20:19 -0700

    Use local environment. Does not work for now.
    As the bind_to_port is not false as expected, will use kube env.
    
    ```
    $ curl 127.0.0.1:41673/config_dump | grep bind_to_port
             "bind_to_port": true
             "bind_to_port": true
    ```
    
    The next step is to use kube env and verify bind_to_port.
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- tests/integration/pilot/filterchain/filterchain_test.go
+++ tests/integration/pilot/filterchain/filterchain_test.go
@@ -1,18 +1,20 @@
 package filterchain
 
 import (
+	"fmt"
 	"istio.io/istio/pkg/test/framework"
 	"istio.io/istio/pkg/test/framework/components/apps"
 	"istio.io/istio/pkg/test/framework/components/environment"
 	pilotcomponent "istio.io/istio/pkg/test/framework/components/pilot"
+	"istio.io/istio/pkg/test/util/retry"
 	"testing"
+	"time"
 )
-
 
 func TestMain(m *testing.M) {
 	framework.Main("filterchain_test", m)
 	// TODO
-	// enforce kube
+	// No need to enforce kube for filter chain
 	// set up envoy
 	// set up client and app
 	// set up pilot
@@ -22,18 +24,41 @@ func TestFoo(t *testing.T) {
 	ctx := framework.NewContext(t)
 	defer ctx.Done(t)
 
-	// TODO kube only
 	ctx.RequireOrSkip(t, environment.Native)
 
-	t.Logf("Before pilot")
 	pilot := pilotcomponent.NewOrFail(t, ctx, pilotcomponent.Config{})
 
-	t.Logf("After pilot")
 	applications := apps.NewOrFail(ctx, t, apps.Config{Pilot: pilot})
 
-	t.Logf("Before Get app A")
-	appA := applications.GetAppOrFail("a", t)
+	aApp := applications.GetAppOrFail("a", t)
+	bApp := applications.GetAppOrFail("b", t)
 
-	t.Logf( "appA name : %s\n", appA.Name())
+	for _, e := range bApp.Endpoints() {
+		t.Logf("b end point %s", e)
+	}
+
+	retry.UntilSuccessOrFail(t, func() error {
+		bPort := bApp.EndpointsForProtocol(apps.AppProtocolHTTP)[0]
+		bProtocol := apps.AppProtocolHTTP
+		results, err := aApp.Call(bPort,
+			apps.AppCallOptions{
+				Protocol: apps.AppProtocol(bProtocol),
+			})
+		if err != nil || len(results) == 0 || results[0].Code != "200" {
+			// Addition log for debugging purpose.
+			if err != nil {
+				fmt.Printf("Error: %#v\n", err)
+			} else if len(results) == 0 {
+				fmt.Printf("No result\n")
+			} else {
+				fmt.Printf("Result: %v\n", results[0])
+			}
+			return fmt.Errorf("%s to %s:%d using %s: expected success, actually failed",
+				aApp.Name(), bApp.Name(), bPort, bProtocol)
+		}
+		return nil
+	}, retry.Delay(time.Second), retry.Timeout(10*time.Second))
+
+	t.Logf("stop here")
 
 }
